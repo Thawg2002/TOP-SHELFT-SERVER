59987a6ab97c7043ef93f2ec768171331f66f1b8
Xuan Thang <thangijr@gmail.com>
2024-08-27 00:21:10 +0700

    update

--- src/routers/order.js
+++ src/routers/order.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
-import { createOrder, getOrderById, getOrders } from "../controllers/order";
+import {
+  createOrder,
+  getOrderById,
+  getOrders,
+  updateOrderStatus,
+} from "../controllers/order";
 
 const router = Router();
 
 router.post("/orders", createOrder);
 router.get("/orders", getOrders);
 router.get("/orders/:userId/:orderId", getOrderById);
+router.put("/orders/:orderId/status", updateOrderStatus);
+
 export default router;
